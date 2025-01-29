4d75b6dace0f6e09521961b8cd33526c7aa73890
lisvargasg <lvarga6@emory.edu>
2025-01-29 10:02:13 -0500

    sixth commit

--- data-code/data-build.R
+++ data-code/data-build.R
@@ -9,7 +9,6 @@
 # Preliminaries -----------------------------------------------------------
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata)
-
  
  ## Basic contract/plan information
   contract.info=read_csv("ECON 470/Homework 1/1-1/data/input/CPSC_Contract_Info_2015_01.csv",
@@ -31,7 +30,12 @@ pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table
                            parent_org = col_character(),
                            contract_date = col_character()
                          ))
- 
+   contract.info <- contract.info %>%
+    group_by(contractid, planid) %>%
+    mutate(id_count=row_number()) %>%
+    filter(id_count==1) %>%
+    select(-id_count)
+
    ## Enrollments per plan
   enroll.info=read_csv("ECON 470/Homework 1/1-1/data/input/CPSC_Enrollment_Info_2015_01.csv",
                        skip=1,
@@ -49,43 +53,33 @@ pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table
  ## Merge contract info with enrollment info
   plan.data = contract.info %>%
     left_join(enroll.info, by=c("contractid", "planid")) %>%
-    mutate(year=2015)
-    
-  ## Fill in missing fips codes (by state and county)
-  plan.data = plan.data %>%
+    mutate(year=2015) %>%
     group_by(state, county) %>%
-    fill(fips)
-
-  ## Fill in missing plan characteristics by contract and plan id
-  plan.data = plan.data %>%
+    fill(fips) %>%
     group_by(contractid, planid) %>%
-    fill(plan_type, partd, snp, eghp, plan_name)
-  
-  ## Fill in missing contract characteristics by contractid
-  plan.data = plan.data %>%
+    fill(plan_type, partd, snp, eghp, plan_name) %>%
     group_by(contractid) %>%
     fill(org_type,org_name,org_marketing_name,parent_org)
 
-  ## Data build
-   saveRDS(plan.data, "ECON 470/Homework 1/1-1/data/output/plan_data.rds")
+saveRDS(plan.data, "ECON 470/Homework 1/1-1/data/output/plan_data.rds")
 
 
-    ## Pull service area data by contract/month
-    service.area=read_csv("ECON 470/Homework 1/1-1/data/input/MA_Cnty_SA_2015_01.csv",skip=1,
-                          col_names=c("contractid","org_name","org_type","plan_type","partial","eghp",
+ ## Pull service area data by contract/month
+  service.area=read_csv("ECON 470/Homework 1/1-1/data/input/MA_Cnty_SA_2015_01.csv",skip=1,
+      col_names=c("contractid","org_name","org_type","plan_type","partial","eghp",
                                       "ssa","fips","county","state","notes"),
-                          col_types = cols(
-                            contractid = col_character(),
-                            org_name = col_character(),
-                            org_type = col_character(),
-                            plan_type = col_character(),
-                            partial = col_logical(),
-                            eghp = col_character(),
-                            ssa = col_double(),
-                            fips = col_double(),
-                            county = col_character(),
-                            notes = col_character()
-                          ), na='*')
+      col_types = cols(
+      contractid = col_character(),
+      org_name = col_character(),
+      org_type = col_character(),
+      plan_type = col_character(),
+      partial = col_logical(),
+      eghp = col_character(),
+      ssa = col_double(),
+      fips = col_double(),
+      county = col_character(),
+      notes = col_character()
+                   ), na='*')
 
 service.area = service.area %>%
   mutate(month = 01, year = 2015)
